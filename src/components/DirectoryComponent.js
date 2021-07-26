84b77c86b0cf83df43c2a8a1608b773ce48d206d
Arthur Baker <artbaker82@gmail.com>
2021-07-26 11:55:37 -0700

    Presentational and Container Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,26 +1,12 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import CampsiteInfo from "./CampsiteInfoComponent";
+
 class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCampsite: null,
-    };
-  }
-
-  onCampSiteSelect(campsite) {
-    this.setState({
-      selectedCampsite: campsite,
-    });
-  }
-
   render() {
-    const { selectedCampsite } = this.state;
     const directory = this.props.campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.onCampSiteSelect(campsite)}>
+          <Card onClick={() => this.props.onClick(campsite.id)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -33,7 +19,6 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <CampsiteInfo campsite={selectedCampsite} />
       </div>
     );
   }
