51835e718ee37341cf73684e2f33ef2f6567c511
Arthur Baker <artbaker82@gmail.com>
2021-08-07 14:05:36 -0600

    campsite info component add review

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,15 +1,121 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import {
   Card,
   CardImg,
   CardText,
-  CardImgOverlay,
-  CardTitle,
   CardBody,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleSubmit(values) {
+    this.toggleModal();
+
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+  }
+  render() {
+    return (
+      <Fragment>
+        <Button onClick={this.toggleModal} outline className="fa-lg">
+          <i className="fa fa-pencil">Submit Comment</i>
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="firstName">Rating</Label>
+
+                <Control.select
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                  placeholder="rating"
+                  className="form-control"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="comment">Author</Label>
+
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="author"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="rating">Rating</Label>
+
+                <Control.textarea
+                  model=".text"
+                  id="text"
+                  name="text"
+                  placeholder="text"
+                  className="form-control"
+                />
+              </div>
+              <Button type="submit" color="primary">
+                Submit Rating
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </Fragment>
+    );
+  }
+}
 
 function RenderCampsite({ campsite }) {
   return (
@@ -46,6 +152,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
+        <CommentForm />
       </div>
     );
   }
